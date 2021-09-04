a96107799b88472d372405c77a2632e9d6f98af3
lxh199533 <50018789+lxh199533@users.noreply.github.com>
2021-09-04 19:25:49 +0800

    Delete dashboard directory

--- dashboard/main.py
+++
@@ -1,5 +0,0 @@
-import GetDiamondList
-
-if __name__ == '__main__':
-    prod_list = GetDiamondList.getDiamondList()
-    GetDiamondList.insertIntoDatabase(prod_list, host='', port='', user='', password='', database='')
